2151dc828c41271d7ece90f1fc8798cc76caa0c8
Md Abidur Rahman <128909117+abidurrahman14004@users.noreply.github.com>
2024-01-01 16:41:04 +0600

    Update primalitytest.cpp

--- primalitytest.cpp
+++ primalitytest.cpp
@@ -1,10 +1,10 @@
 bool prime(int n){
-if(n<=2) return false;
+if(n<2) return false;
 if(n<=3) return true;
 if(n%2==0) return false;
 for(int i=2;i<=sqrt(n);i++){
 
-    if(i%2==0) return false;
+    if(n%i==0) return false;
     else return true;
 }
 
